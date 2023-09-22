3a1ce6b3b49533d2ad0e6e84b711f9da399a43f8
MisterZig <107263655+MisterZig@users.noreply.github.com>
2023-09-22 22:01:06 +0200

    Added Quaternion.hpp and updated test.cpp accordingly.

---
+++ quaternion.hpp
@@ -0,0 +1,68 @@
+#ifndef QUATERNION_HPP
+#define QUATERNION_HPP
+
+#include <cmath> // sqrt
+
+template <typename T>
+struct Quaternion {
+    T real;
+    T i;
+    T j;
+    T k;
+};
+
+template <typename T>
+Quaternion<T> operator+(const Quaternion<T>& lhs, const Quaternion<T>& rhs) {
+    return {lhs.real + rhs.real, lhs.i + rhs.i, lhs.j + rhs.j, lhs.k + rhs.k};
+}
+
+template <typename T>
+Quaternion<T> operator-(const Quaternion<T>& lhs, const Quaternion<T>& rhs) {
+    return {lhs.real - rhs.real, lhs.i - rhs.i, lhs.j - rhs.j, lhs.k - rhs.k};
+}
+
+template <typename T>
+Quaternion<T> operator*(const Quaternion<T>& lhs, const Quaternion<T>& rhs) {
+    return {
+        lhs.real * rhs.real - lhs.i * rhs.i - lhs.j * rhs.j - lhs.k * rhs.k,
+        lhs.real * rhs.i + lhs.i * rhs.real + lhs.j * rhs.k - lhs.k * rhs.j,
+        lhs.real * rhs.j - lhs.i * rhs.k + lhs.j * rhs.real + lhs.k * rhs.i,
+        lhs.real * rhs.k + lhs.i * rhs.j - lhs.j * rhs.i + lhs.k * rhs.real
+    };
+}
+
+template <typename T>
+Quaternion<T> operator/(const Quaternion<T>& lhs, const Quaternion<T>& rhs) {
+    return {
+        (lhs.real * rhs.real + lhs.i * rhs.i + lhs.j * rhs.j + lhs.k * rhs.k) /
+            (rhs.real * rhs.real + rhs.i * rhs.i + rhs.j * rhs.j + rhs.k * rhs.k),
+        (lhs.i * rhs.real - lhs.real * rhs.i - lhs.j * rhs.k + lhs.k * rhs.j) /
+            (rhs.real * rhs.real + rhs.i * rhs.i + rhs.j * rhs.j + rhs.k * rhs.k),
+        (lhs.j * rhs.real + lhs.real * rhs.j - lhs.i * rhs.k - lhs.k * rhs.i) /
+            (rhs.real * rhs.real + rhs.i * rhs.i + rhs.j * rhs.j + rhs.k * rhs.k),
+        (lhs.k * rhs.real + lhs.real * rhs.k + lhs.i * rhs.j - lhs.j * rhs.i) /
+            (rhs.real * rhs.real + rhs.i * rhs.i + rhs.j * rhs.j + rhs.k * rhs.k)
+    };
+}
+
+template <typename T>
+Quaternion<T> quaternion_conjugate(const Quaternion<T>& alpha) {
+    return {alpha.real, -alpha.i, -alpha.j, -alpha.k};
+}
+
+template <typename T>
+T abs(const Quaternion<T>& alpha) {
+    return sqrt(alpha.real * alpha.real + alpha.i * alpha.i + alpha.j * alpha.j + alpha.k * alpha.k);
+}
+
+template <typename T>
+Quaternion<T> sqrt(const Quaternion<T>& alpha) {
+    return {
+        sqrt((alpha.real + abs_quaternion(alpha)) / 2),
+        sqrt((-1 * alpha.real + abs_quaternion(alpha)) / 2),
+        sqrt((-1 * alpha.real + abs_quaternion(alpha)) / 2),
+        sqrt((-1 * alpha.real + abs_quaternion(alpha)) / 2)
+    };
+}
+
+#endif // QUATERNION_HPP
--- test.cpp
+++ test.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "Cnumbers.hpp"
+#include "quaternion.hpp"
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
     cout << "This is a test of the Cnumbers class." << endl;
-    cout << "(3 + 4i) / (4 - 2i) = ";
+    cout << "(3 + 4i) / (4 - 2i) = "; // should be 0.2 + 1.1i
     Complex<double> calc = (Complex<double>({3, 4}) / Complex<double>({4, -2}));
-    cout << abs_complex(calc) << endl;
+    cout << calc.real << " + " << calc.imaginary << "i" << endl;
+    cout << "=> |(3 +4i) / (4 - 2i)| = "; // should be 1.11803
+    cout << abs(calc) << endl;
     //cout << calc.real << " + " << calc.imaginary << "i" << endl;
+
+    cout << "This is a test of the Quaternion class." << endl;
+    cout << "i*j*k = "; // should be -1
+    Quaternion<double> calc2 = Quaternion<double>({0, 1, 0, 0}) * Quaternion<double>({0, 0, 1, 0}) * Quaternion<double>({0, 0, 0, 1});
+    cout << calc2.real << " + " << calc2.i << "i + " << calc2.j << "j + " << calc2.k << "k" << endl;
     return 0;
 }
